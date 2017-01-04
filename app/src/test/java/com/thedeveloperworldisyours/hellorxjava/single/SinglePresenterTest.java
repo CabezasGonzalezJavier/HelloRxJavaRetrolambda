b7d88b82d08e87d824fa68c2dfaa8d8a7cdb3e40
CabezasGonzalezJavier <javiergonzalezcabezas@gmail.com>
2017-01-04 17:27:28 +0000

    added junit test in Single

--- app/src/test/java/com/thedeveloperworldisyours/hellorxjava/single/SinglePresenterTest.java
+++ app/src/test/java/com/thedeveloperworldisyours/hellorxjava/single/SinglePresenterTest.java
@@ -5,8 +5,18 @@ import com.thedeveloperworldisyours.hellorxjava.simple.singles.SinglesContract;
 import com.thedeveloperworldisyours.hellorxjava.simple.singles.SinglesPresenter;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import io.reactivex.Single;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by javierg on 15/12/2016.
@@ -14,6 +24,8 @@ import org.mockito.MockitoAnnotations;
 
 public class SinglePresenterTest {
 
+    @Mock
+    Callable<List<String>> mCallable;
 
     @Mock
     RestClient mRestClient;
@@ -21,12 +33,45 @@ public class SinglePresenterTest {
     @Mock
     SinglesContract.View mView;
 
-    SinglesPresenter mPresenter;
+    private static List<String> sShowTV;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
 
-        mPresenter = new SinglesPresenter(mView, mRestClient);
+        sShowTV = new ArrayList<>();
+        sShowTV.add("The Joy of Painting");
+        sShowTV.add("The Simpsons");
+        sShowTV.add("Futurama");
+        sShowTV.add("Game of Thrones");
+        sShowTV.add("Law And Order");
+
+    }
+
+    @Test
+    public void successful() throws Exception {
+
+        when(mCallable.call()).thenReturn(sShowTV);
+        Single.fromCallable(mCallable)
+                .subscribe((List<String> tvShows) -> mView.displayTvShows(tvShows),
+                        (Throwable error) ->  mView.displayErrorMessage());
+
+        verify(mView).displayTvShows(sShowTV);
+        verify(mCallable).call();
+
+    }
+
+    @Test
+    public void fail() throws Exception {
+        Throwable error = new IllegalStateException();
+
+        when(mCallable.call()).thenThrow(error);
+
+        Single.fromCallable(mCallable)
+                .subscribe((List<String> tvShows) -> mView.displayTvShows(tvShows),
+                        (Throwable errorSubscribe) ->  mView.displayErrorMessage());
+
+        verify(mView).displayErrorMessage();
+        verify(mCallable).call();
     }
 }
